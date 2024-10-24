ff3e9205846c8122a97afcbe720e93f9906ec9cb
SHERRY699 <shehryarnasir98@gmail.com>
2024-10-25 00:28:45 +0500

    'chnges'

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ Db();
 //middlewares
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://pet-app-backend-production.up.railway.app",
+      "http://localhost:4000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 //routes
 app.use("/user", userRouter);
